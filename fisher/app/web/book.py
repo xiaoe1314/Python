c65e1414dbf21096f276f9b05f2720bccff025d1
xiaoe <1945775580@qq.com>
2018-12-06 01:20:07 +0800

    优化url出现空格访问不到的bug

--- fisher/app/web/book.py
+++ fisher/app/web/book.py
@@ -6,6 +6,7 @@ from flask import jsonify
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 from . import web
+import json
 
 
 # <q>/<page> 添加参数
@@ -15,12 +16,14 @@ def search(num):
         q: 普通关键词 isbn
         page:
     """
-    isbn_or_key = is_isbn_or_key(num)
+    # 兼容用户在前后输入空格
+    num_key = num.strip()
+    isbn_or_key = is_isbn_or_key(num_key)
     if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(num)
+        result = YuShuBook.search_by_isbn(num_key)
     else:
-        result = YuShuBook.search_by_keyword(num)
+        result = YuShuBook.search_by_keyword(num_key)
         # dict 序列化
         # API
-    return jsonify(result)
-    # return json.dumps(result), 200, {'content-type': 'application/json'}
+    # return jsonify(result)
+    return json.dumps(result, ensure_ascii=False), 200, {'content-type': 'application/json'}
